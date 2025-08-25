6f370cb4f70655f0459b11ece70b442a52af7108
Md Faisal Yousuf Afrid <mdfaisalafrid@gmail.com>
2025-08-25 16:50:42 +0600

    am

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var navToggler = document.querySelector(".navbar-toggler"),
+navTogglerIcon = document.querySelector(".navbar-toggler-icon");
+navToggler.addEventListener("click", () => {
+    navTogglerIcon.classList.toggle("close-icon");
+})
+
+
