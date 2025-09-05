18573b5b905899a8c790d8299e4f20abf2d97532
Md Faisal Yousuf Afrid <mdfaisalafrid@gmail.com>
2025-09-05 16:31:14 +0600

    am

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
 var navToggler = document.querySelector(".navbar-toggler"),
-navTogglerIcon = document.querySelector(".navbar-toggler-icon");
+navTogglerIcon = document.querySelector(".navbar-toggler-icon"),
+allSelectOpt = document.querySelectorAll('.select-options li'),
+groupselect = document.querySelector('.select-options'),
+selectDept= document.querySelector(".select");
 navToggler.addEventListener("click", () => {
     navTogglerIcon.classList.toggle("close-icon");
 })
 
+selectDept.addEventListener('click', () => {
+    groupselect.classList.toggle("active");
+})
 
+allSelectOpt.forEach((opt) => {
+    opt.addEventListener("click", () => {
+            allSelectOpt.forEach((option) => {
+                option.classList.remove("activated");
+                option.innerHTML = `<p>${option.getAttribute("data-value")}</p>`;
+            })
+            opt.classList.toggle('activated');
+            if(opt.classList.contains('activated')){
+                opt.innerHTML = `
+                <p>${opt.getAttribute("data-value")}</p>
+                <div class="icon">
+                    <i class="fi fi-rs-check"></i>
+                </div>
+                `;
+            }
+            else{
+                opt.innerHTML = `<p>${opt.getAttribute("data-value")}</p>`;
+            }
+    })
+})
